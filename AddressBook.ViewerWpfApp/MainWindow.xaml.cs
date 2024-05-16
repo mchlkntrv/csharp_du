8d4eb5ea06d3e16a5532ce0d7cb4a8c70282ce2c
MICHAELA\micha <michaela.kantorova@gmail.com>
2024-05-16 17:18:52 +0200

    projekt 3 done

--- AddressBook.ViewerWpfApp/MainWindow.xaml.cs
+++ AddressBook.ViewerWpfApp/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using AddressBook.CommonLibrary;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows;
 
 namespace AddressBook.ViewerWpfApp
 {
@@ -7,9 +10,72 @@ namespace AddressBook.ViewerWpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private EmployeeList? _employeeList;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFileClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var fileInfo = new FileInfo(openFileDialog.FileName);
+                _employeeList = EmployeeList.LoadFromJson(fileInfo);
+                if (_employeeList != null)
+                {
+                    PositionComboBox.ItemsSource = _employeeList.GetPositions();
+                    WorkplaceComboBox.ItemsSource = _employeeList.GetMainWorkplaces();
+                }
+            }
+        }
+
+        private void SearchClick(object sender, RoutedEventArgs e)
+        {
+            if (_employeeList != null)
+            {
+                var results = _employeeList.Search(
+                    mainWorkplace: WorkplaceComboBox.SelectedItem?.ToString(),
+                    position: PositionComboBox.SelectedItem?.ToString(),
+                    name: NameTextBox.Text);
+
+                EmployeesListBox.ItemsSource = results.Employees;
+                EmployeeCountLabel.Content = results.Employees.Length.ToString();
+            }
+        }
+
+        private void ResetClick(object sender, RoutedEventArgs e)
+        {
+            PositionComboBox.SelectedIndex = -1;
+            WorkplaceComboBox.SelectedIndex = -1;
+            NameTextBox.Clear();
+            EmployeesListBox.ItemsSource = null;
+            EmployeeCountLabel.Content = "0";
+        }
+
+        private void ExportClick(object sender, RoutedEventArgs e)
+        {
+            if (_employeeList != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*"
+                };
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    var results = _employeeList.Search(
+                        mainWorkplace: WorkplaceComboBox.SelectedItem?.ToString(),
+                        position: PositionComboBox.SelectedItem?.ToString(),
+                        name: NameTextBox.Text);
+
+                    var fileInfo = new FileInfo(saveFileDialog.FileName);
+                    results.SaveToCsv(fileInfo, ";");
+                }
+            }
         }
     }
 }
